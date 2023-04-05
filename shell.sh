343c302c615f1a115a556d0e321dc1839cda1231
Chishin <zhangzejin3883@163.com>
2023-04-05 12:09:33 +0800

    添加测试脚本

---
+++ shell.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "BUILD_TAG=$BUILD_TAG"
+echo "BUILD_IMAGE=$BUILD_IMAGE"
+echo "BUILD_PLATFORM=$BUILD_PLATFORM"
+echo "BUILD_VERSION=$BUILD_VERSION"
+
+if [ $BUILD_TAG ];then
+	echo "BUILD_TAG OK"
+else
+	echo "BUILD_TAG is not found"
+fi
