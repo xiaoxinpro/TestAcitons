4855e4c12508f6ba3022695b437ccbd827b7a25c
Chishin <zhangzejin3883@163.com>
2023-04-05 12:20:49 +0800

    重构环境变量是否存在判断

--- shell.sh
+++ shell.sh
@@ -4,7 +4,7 @@ echo "BUILD_IMAGE=$BUILD_IMAGE"
 echo "BUILD_PLATFORM=$BUILD_PLATFORM"
 echo "BUILD_VERSION=$BUILD_VERSION"
 
-if [ "$BUILD_TAG" ]; then
+if [ 0"$BUILD_TAG" != "0" ]; then
   echo "BUILD_TAG OK"
 else
   echo "BUILD_TAG is not found"
