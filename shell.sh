0663897f37b50aae09073c59f301c24f3e65b753
Chishin <zhangzejin3883@163.com>
2023-04-05 12:26:48 +0800

    测试脚本

--- shell.sh
+++ shell.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+
+if [ -n "$BUILD_TAG" ]; then
+  echo "BUILD_TAG OK"
+else
+  echo "BUILD_TAG is not found"
+fi
+
 echo "BUILD_TAG=$BUILD_TAG"
 echo "BUILD_IMAGE=$BUILD_IMAGE"
 echo "BUILD_PLATFORM=$BUILD_PLATFORM"
 echo "BUILD_VERSION=$BUILD_VERSION"
 
-if [ 0"$BUILD_TAG" != "0" ]; then
-  echo "BUILD_TAG OK"
-else
-  echo "BUILD_TAG is not found"
-fi
